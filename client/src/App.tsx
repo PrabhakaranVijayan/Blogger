923658405ae4344bec0daa0d1ecc5a6f2fa46d89
prabhakaran <prabhakarvijayan24@gmail.com>
2024-03-06 11:54:28 +0530

    pages added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,22 +2,25 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import './App.css'
 import Signup from './pages/signup'
 import Landing from './pages/landing'
+import Signin from './pages/signin'
+import Home from './pages/home'
 
 function App() {
   
 
   return (
-    <div>
+    
      
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Landing />} />
       <Route path='/signup' element={<Signup />} />
-      
+      <Route path='/signin' element={<Signin />} />
+      <Route path='/home' element={<Home />} />
     </Routes>
     </BrowserRouter>
 
-    </div>
+  
     
   )
 }
--- client/src/pages/signup.tsx
+++ client/src/pages/signup.tsx
@@ -1,5 +1,15 @@
 export default function Signup(){
     return(
-        <div>Signup</div>
+        <div className="border w-1/4 flex  justify-center items-center h-64 rounded mx-auto my-10  ">
+            <div className=" flex flex-col justify-center h-full space-y-4">
+             <input type="text" placeholder="username" className="border "></input>
+             
+             <input type="text" placeholder="E-mail Id" className="border "></input>
+             
+             <input type="text" placeholder="password" className="border"></input>
+             
+             <button  className="border rounded inline-block ">signup</button>
+            </div>
+        </div>
     )
 }
